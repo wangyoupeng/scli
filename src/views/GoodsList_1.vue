e95f81aebd50b5224640d7431198e117a4e9f54d
wyp <youpeng.wang@marsmarch.cn>
2023-07-11 17:34:43 +0800

    goods list ....

--- src/views/GoodsList_1.vue
+++ src/views/GoodsList_1.vue
@@ -11,7 +11,19 @@
       <div class="groupandgoods-cont">
         <el-col :span="5">
           <div class="grid-content bg-purple groupslist">
-            分类
+            <el-menu
+              :default-active="activeCategory"
+              @select="handleCategorySelect"
+              style="width: 100%;"
+            >
+              <el-menu-item style="padding:0px;"
+                v-for="(category, index) in categories"
+                :key="index"
+                :index="category.value"
+              >
+                {{ category.label }}
+              </el-menu-item>
+            </el-menu>
           </div>
         </el-col>
         <el-col :span="19">
@@ -31,7 +43,38 @@
   export default {
     components: {
       Banner,
-    }
+    },
+    data(){
+      return {
+        activeCategory: '',
+        categories: [
+          { label: '分类1', value: 'category1' },
+          { label: '分类2', value: 'category2' },
+          { label: '分类3', value: 'category3' },
+          { label: '分类4', value: 'category4' },
+          { label: '分类5', value: 'category5' },
+          { label: '分类6', value: 'category6' },
+          { label: '分类7', value: 'category7' },
+          { label: '分类8', value: 'category8' },
+          { label: '分类9', value: 'category9' },
+          { label: '分类10', value: 'category10' },
+        ],
+      }
+    },
+    computed: {
+      filteredProducts() {
+        if (this.activeCategory) {
+          return this.products.filter(product => product.category === this.activeCategory);
+        }
+        return this.products;
+      },
+    },
+    methods: {
+      handleCategorySelect(category) {
+        this.activeCategory = category;
+      },
+    },
+
   }
 </script>
 
